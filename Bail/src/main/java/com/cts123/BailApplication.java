45d979fb69231bda07ab33cd22eb7c989bfbe6b5
shihaji <shihajig@gmail.com>
2021-03-20 08:13:56 +0530

    commited

---
+++ Bail/src/main/java/com/cts123/BailApplication.java
@@ -0,0 +1,13 @@
+package com.cts123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BailApplication.class, args);
+	}
+
+}
